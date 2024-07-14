7c3f383639863e7bed08581a9f74552a179bdfad
kan kawabata <kkawabat@asu.edu>
2024-07-14 10:11:00 -0700

    added business logic of the telecog assessment (working)

--- telecog_main/urls.py
+++ telecog_main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from telecog_main.views import greetings, analyze_object_naming, assessment1
+from telecog_main.views import greetings, analyze_object_naming, assessment1, assessment2
 
 urlpatterns = [
     path('greetings', greetings),
     path('assessment1', assessment1),
-    path('assessment2', assessment1),
+    path('assessment2', assessment2),
     path('analyze_object_naming', analyze_object_naming),
 ]
--- telecog_main/views.py
+++ telecog_main/views.py
@@ -11,7 +11,7 @@ def greetings(request):
     resp = VoiceResponse()
 
     gather = Gather(num_digits=1, action='assessment1')
-    gather.say("Welcome to Linus Telephone cognition screening, if you are interested in taking our 5 minute cognitive assessment on the phone please press 1.")
+    gather.say("Welcome to Linus Telephone cognition screening, if you are interested in taking our 1 minute cognitive assessment on the phone please press 1.")
     resp.append(gather)
     return HttpResponse(str(resp))
 
@@ -31,11 +31,12 @@ def assessment1(request):
 
         resp.say("Great! Please note that the call will be recorded in order to assess your responses. "
                  "We will begin this assessment with an object naming test, "
-                 "after the beep please name as many object as you can think of that starts with the letter B")
-        resp.record(action='/assessment2', method='GET', timeout=20,
-                    maxLength=60,
+                 "after the beep please name as many object as you can think of that starts with the letter, B.")
+        resp.record(action='assessment2', method='POST', timeout=60,
+                    maxLength=20,
                     transcribe=True, transcribeCallback='analyze_object_naming',
                     playBeep=True)
+        resp.say("Great you have finished the assessment!")
     else:
         resp.say("To learn more information about our screening please visit us at our website linus dot health, thank you.")
 
@@ -44,18 +45,23 @@ def assessment1(request):
 
 @csrf_exempt
 def analyze_object_naming(request):
-    ass = Assessment.objects.get(sid=request.form['CallSid'])
-    ass.update(object_naming_result=3)
+    ass = Assessment.objects.get(call_sid=request.POST['CallSid'])
+    text = request.POST['TranscriptionText']
+    print(text)
+    ass.object_naming_result = len([t for t in text.split() if t.lower().startswith('b')])
+    ass.save()
+    return HttpResponse(status=200)
 
 
 @csrf_exempt
 def assessment2(request):
     resp = VoiceResponse()
 
-    ass = Assessment.objects.get(sid=request.form['CallSid'])
+    ass = Assessment.objects.get(call_sid=request.POST['CallSid'])
     if ass.object_naming_result is None:
-        resp.say("Great you have finished the assessment! Please wait while we process your responses")
-        resp.redirect('/assessment2')
+        resp.say("Please wait a moment while we process your responses, this might take a moment")
+        resp.pause(length=5)
+        resp.redirect('assessment2')
     else:
 
         resp.say(f"Your results are in you scored {ass.object_naming_result} on the object naming test.")
